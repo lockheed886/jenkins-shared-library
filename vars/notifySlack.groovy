8717f177c32ab2401ce4ad9aedd33b05b8188f03
lockheed886 <lockheed886@users.noreply.github.com>
2026-04-28 19:13:36 +0500

    Update notifySlack with foolproof code

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,3 +1,10 @@
-def call(Map config = [:]) {
-    slackSend channel: '#devops-notifications', message: config.message
+def call(Map params = [:]) {
+    // Line 2: Safely check if a message was provided
+    def msg = params.get('message', 'Default Build Notification')
+    
+    // Print to Jenkins console
+    echo "Sending Slack Notification: ${msg}"
+    
+    // Call the actual Jenkins Slack plugin
+    slackSend(color: 'GOOD', message: msg)
 }
