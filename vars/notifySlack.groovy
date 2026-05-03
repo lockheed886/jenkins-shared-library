650017e98b7b5929843e2494780f98d572f5b18a
lockheed886 <lockheed886@users.noreply.github.com>
2026-05-04 00:49:48 +0500

    Step 1: Neutralize Slack step to prevent credential crashes

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,10 +1,9 @@
 def call(Map params = [:]) {
-    // Line 2: Safely check if a message was provided
     def msg = params.get('message', 'Default Build Notification')
     
-    // Print to Jenkins console
+    // Print to Jenkins console 
     echo "Sending Slack Notification: ${msg}"
     
-    // Call the actual Jenkins Slack plugin
-    slackSend(color: 'GOOD', message: msg)
+    // Commented out to prevent credential crashes
+    // slackSend(color: 'GOOD', message: msg)
 }
